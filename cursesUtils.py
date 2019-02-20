4dcdf3a8d28c0e2f23e8a46bc6cec018e14094af
Henry Seed <henryseed@Henrys-MBP.lan>
2019-02-21 08:38:36 +1300

    adds colors and resizing

--- cursesUtils.py
+++ cursesUtils.py
@@ -23,3 +23,7 @@ def setupColour(win):
     curses.use_default_colors()
     curses.init_pair(0, 0, -1)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_RED)
+    curses.init_pair(2, curses.COLOR_GREEN, -1)
+    curses.init_pair(3, curses.COLOR_YELLOW, -1)
+
+
