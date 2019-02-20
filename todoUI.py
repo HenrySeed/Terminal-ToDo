476fbcbbc5f133dfdcda4db6817276ce859f6b9b
Henry Seed <henryseed@Henrys-MBP.lan>
2019-02-21 12:34:44 +1300

    adds better list coloring

--- todoUI.py
+++ todoUI.py
@@ -70,28 +70,30 @@ def print_todo_line(win, y, x, inputLineStr, checked, done):
 
     lines = get_split_todo(inputLineStr)
 
-    # win.addstr(25, 0, " " * 400)
-    # win.addstr(25, 0, str(lines))
-    
-    count = 0
-    for line in lines:
-        if done:
-            if count == 0:
-                win.addstr(y+count, x, "[x] " + line, curses.color_pair(2))
-            else:
-                win.addstr(y+count, x, "    " + line, curses.color_pair(2))
-        else:
-            if count == 0:
-                win.addstr(y+count, x, "[ ] " + line, curses.color_pair(3))
-            else:
-                win.addstr(y+count, x, "    " + line, curses.color_pair(3))
-                
-        
+   # win.addstr(25, 0, " " * 400)
+   # win.addstr(25, 0, str(lines))
 
-        count += 1
+   if done:
+       text_color = curses.color_pair(2)
+       tick_color = curses.color_pair(2)
+       tick = "[x]"
+   else:
+       text_color = curses.color_pair(3)
+       tick_color = curses.color_pair(3)
+       tick = "[ ]"
 
-    # re return the number of lines so we can move the next one down
-    return count
+
+   count = 0
+   for line in lines:
+       if count == 0:
+           win.addstr(y+count, x, tick, tick_color)
+
+       win.addstr(y+count, x+5, line, text_color)
+
+       count += 1
+
+   # re return the number of lines so we can move the next one down
+   return count
 
 
 def print_new_todo_input(win, todo, tbCursor, active, linesUsed):
